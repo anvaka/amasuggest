2cc77dfcca4edc95cb2debf745f629636028ac0d
anvaka <anvaka@gmail.com>
2014-03-31 02:30:00 -0700

    Added transform function

--- flatten.js
+++ flatten.js
@@ -1,12 +1,22 @@
-module.exports = function flatten(papiItem) {
-  var image = 'http://images.amazon.com/images/P/' + papiItem.ASIN[0] + '.01.jpg';
-  return {
-    asin: papiItem.ASIN[0],
-    url: papiItem.DetailPageURL[0],
-    image: image,
-    title: papiItem.ItemAttributes[0].Title[0],
-    toString: function () {
-      return this.title;
-    }
+module.exports = function (transform) {
+  if (typeof transform !== 'function') {
+    transform = function () {}; // noop;
+  }
+
+  return function flatten(papiItem) {
+    var image = 'http://images.amazon.com/images/P/' + papiItem.ASIN[0] + '.01.jpg';
+    var product = {
+      asin: papiItem.ASIN[0],
+      url: papiItem.DetailPageURL[0],
+      image: image,
+      title: papiItem.ItemAttributes[0].Title[0],
+      toString: function () {
+        return this.title;
+      }
+    };
+
+    transform(papiItem, product);
+
+    return product;
   };
 };
--- index.js
+++ index.js
@@ -2,19 +2,20 @@ require('typeahead.an');
 require('an').directive(amazonSearch);
 
 var fs = require('fs');
-var flatten = require('./flatten');
 
 function amazonSearch($http) {
   return {
     restrict:'EA',
     scope:{
       src:'=',
+      transform: '='
     },
     replace:true,
     template: fs.readFileSync(__dirname + '/index.html', 'utf8'),
     link: function (scope, element, attrs) {
+      var flatten = require('./flatten')(scope.transform);
       scope.getProduct = function (val) {
-        return $http.get(attrs.src || 'http://amasearch.herokuapp.com/', {
+        return $http.get(scope.src || 'http://amasearch.herokuapp.com/', {
           params: { keywords:val }
         }).then(function (res) {
           if (res.data) return res.data.map(flatten);
